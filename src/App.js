38d6728c76ef328e57636a3938ea0b7fb651399f
Jerwin Cruz <cruz.jerwin15@gmail.com>
2023-01-10 14:22:34 +0800

    Section Title draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Hero from './components/Hero/Hero';
+import SectionTitle from './components/SectionTitle/SectionTitle';
 
 
 function App() {
   return (
     <div>
       <Hero/>
+      <SectionTitle/>
     </div>
   );
 }
---
+++ src/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './SectionTitle.css';
+const SectionTitle=()=> {
+    return (
+        <React.Fragment>
+            <div className="section-title">
+                
+                <h2 >Skills</h2>
+                <div class="left-title"></div>
+                <div class="right-title"></div>
+            </div>
+            
+        </React.Fragment>
+    );
+}
+
+export default SectionTitle;
